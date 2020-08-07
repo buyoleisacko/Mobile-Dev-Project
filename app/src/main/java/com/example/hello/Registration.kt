b530484013e9087875e49340ce24fe552b9cadfa
isackouyole <isackobuyole@gmail.comgit>
2020-08-07 16:55:33 +0300

    commit

--- app/src/main/java/com/example/hello/Registration.kt
+++ app/src/main/java/com/example/hello/Registration.kt
@@ -1,11 +1,37 @@
 package com.example.hello
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+import android.support.v7.app.AppCompatActivity as AppCompatActivity1
 
-class Registration : AppCompatActivity() {
+class Registration : AppCompatActivity1() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+    }
+}
+package com.example.firstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.etPassword
+import kotlinx.android.synthetic.main.activity_main.tvRegister
+import kotlinx.android.synthetic.main.activity_registrationactivity.*
+
+class Registrationactivity : AppCompatActivity1() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrationactivity)
+        tvRegister.setOnClickListener {
+            var FirstName=etFirstName.text.toString()
+            var Password= etPassword.text.toString()
+            var LastName=etLastName.text.toString()
+            var PhoneNumber=etPhoneNumber.text.toString()
+            var ConfirmPassword=etPassword.text.toString()
+            Toast.makeText(baseContext,Password, Toast.LENGTH_LONG).show()
+
+
+        }
     }
 }
